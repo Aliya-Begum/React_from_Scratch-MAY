3e745b11f064c531337f2848a40e8f6cf3b2c502
Aliya Begum <aliyaansari0604@gmail.com>
2024-05-21 23:44:57 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  {},
+  "Episode-1 Inception(using React CDN)"
+);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
